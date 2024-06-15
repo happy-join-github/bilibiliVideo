4989bd50875650fc2a65ab605d1c72b7b3657745
happy-join-github <123853776+happy-join-github@users.noreply.github.com>
2024-06-15 15:32:13 +0800

    updata config.py

--- bilibiliV2.0/BackEnd/App/config.py
+++ bilibiliV2.0/BackEnd/App/config.py
@@ -2,4 +2,5 @@ class Conf:
     datasetsUrl = './App/static/user.json'
     setcookiesPath = './App/static/cookies.json'
     getcookiesPath = './App/static/cookies.txt'
+    userShowname = ''
     
